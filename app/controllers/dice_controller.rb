41a3c1451d65a3b5b422277aa13fbb73346d7639
dariushinojosa <101020189+dariushinojosa@users.noreply.github.com>
2024-10-28 21:50:06 +0000

    first try

---
+++ app/controllers/dice_controller.rb
@@ -0,0 +1,21 @@
+class DiceController < ApplicationController
+
+  def home_action
+    render({ :template => "game_templates/home"})
+  end
+
+  def dice_action
+    
+    @num_dice = params.fetch("number_of_dice").to_i
+
+    @num_sides = params.fetch("how_many_sides").to_i
+  
+    @rolls = []
+
+    @num_dice.times do
+      die = rand(1..@num_sides)
+      @rolls.push(die)
+  
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "dice", :action => "home_action"})
+
+  get("/dice/:number_of_dice/:how_many_sides", { :controller => "dice", :action => "dice_action"})
+
 end
